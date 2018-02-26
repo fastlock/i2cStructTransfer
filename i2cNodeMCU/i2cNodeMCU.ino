c17a2f5817037097e1c18544eac9c8f774d823e3
fastlock <marco.mrf@gmail.com>
2018-02-26 09:11:04 +0100

    Commenti più leggibili

--- i2cNodeMCU/i2cNodeMCU.ino
+++ i2cNodeMCU/i2cNodeMCU.ino
@@ -1,28 +1,45 @@
-#include <I2C_Anything.h>
+/* MASTER
+ * Esempio di "Master" che legge i dati da uno "Slave"
+ * il ruolo di di questa periferica è di interrogare ad un indirizzo
+ *  di un altro dispositivo connesso alla linea i2c
+ * ---------------------------------------------------
+ * CONNESSIONI:
+ * NODE MCU->ARDUINO UNO
+ * SDA:D1->A4(Analog pin)
+ * SCL:D2->A5(Analog pin)
+ * GND->GND(il ground DEVE essere condiviso)
+*/
 
-//Master Reader
+#include <I2C_Anything.h>
 #include<Wire.h>
 
+#define SlaveAddress 8 //definisco indirizzo periferica
 
 void setup() {
-  Wire.begin(D1, D2); // sda, scl
-  Serial.begin(9600);  // start serial for output
+  Wire.begin(D1, D2); // inizializzo i pin sda, scl per la comunicazione
+  Serial.begin(9600);  // inizializzo la comunicazione seriale solo per DEBUG
 }
+/*
+ * Questa è la struttura dati da inviare
+ * qui si inserisce il prototipo del pacchetto che riceverà il master
+ * ##IMPORTANTE##
+ * MASTER E SLAVE DEVONO AVERE LO STESSO PROTOTIPO
+ */
 struct SensorPacket{
     float Temp;
     float lum;
     float noise;
   };
-SensorPacket myData;
+SensorPacket myData;//creo instanza della struttura 
+
 void loop(){
-  Serial.println("--------------------");
-   Wire.requestFrom(8, sizeof(SensorPacket));    // request SensorPacket bytes from slave device #8
   
+   Wire.requestFrom(SlaveAddress, sizeof(SensorPacket));    // Richiedo struttura allo slave (attenzione a come è fatta la funzione:Wire.requestFrom(indirizzo slave,byte da ricevere) 
     delay(100);
-    I2C_readAnything (myData);
+    I2C_readAnything (myData);//quello che leggo riempie la mia struttura "vuota"
     
 
-  
+  //DEBUG
   Serial.println("----------");
   Serial.print("TEMPERATURA:\t");
   Serial.println(myData.Temp);
@@ -32,6 +49,7 @@ void loop(){
   Serial.println(myData.noise);
   Serial.println("----------");
   delay(1000);
+  
 
 }
 
--- i2cUNO/i2cUNO.ino
+++ i2cUNO/i2cUNO.ino
@@ -1,30 +1,39 @@
-//slave sender
+//Esempio di "slave" che invia i dari solo su richiesta del master
 #include <Wire.h>
 #include <I2C_Anything.h>
 void setup() {
   Serial.begin(9600);
-  Wire.begin(8);                // join i2c bus with address #8
-  Wire.onRequest(requestEvent); // register event
+  Wire.begin(8);                // Inizializzo lo slave con l'indirizzo di linea #8
+  Wire.onRequest(requestEvent); // alla richiesta esegui la funzione requestEvent
 }
+/*
+ * Questa è la struttura dati da inviare
+ * qui si inserisce il prototipo del pacchetto che riceverà il master
+ * ##IMPORTANTE##
+ * MASTER E SLAVE DEVONO AVERE LO STESSO PROTOTIPO
+ */
 struct SensorPacket{
-    float Temp;
+    float Temp;//definisco le variabili da inviare
     float lum;
     float noise;
   };
-SensorPacket myData;
+SensorPacket myData;//creo un instanza della struttura 
 void loop() {
-  myData.Temp=18.24;
+  myData.Temp=18.24;//riempio la struttura con dei dati finti
   myData.lum=2.3;
   myData.noise=200.01;
   //delay(100);
 }
 
-// function that executes whenever data is requested by master
-// this function is registered as an event, see setup()
+/*
+ * La funzione requestEvent() viene eseguita se
+ * sulla linea,il master interroga l'indirizzo impostato
+ * nel setup()
+ */
 void requestEvent() {
   int i=0;
   Serial.println("sending data");
-    I2C_writeAnything (myData);
+    I2C_writeAnything (myData);//con questa funzione invio alla linea la struttura già preparata
   
   // as expected by master
 }
